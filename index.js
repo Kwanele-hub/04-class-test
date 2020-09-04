d86928928a03048ce5bea3a80ba72ca66c02f6ee
nolwazi749 <nolwazizwane7@gmail.com>
2020-09-04 04:21:06 -0400

    the palindrome code that returns false

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function palindrome(str) {
     var re = /[\W_]/gi;
-    
+    var lowRegStr = str.toLowerCase().replace(re, '');
+    var reverseStr = lowRegStr.split('').reverse().join(''); 
+    return reverseStr === lowRegStr; 
+  }
+   
+  console.log(palindrome("A dog, a plan, a canal. Panama"));
